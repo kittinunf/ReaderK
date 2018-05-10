2033108d44ae7b807d4b9fe7953d94a5bb9c7f68
Kittinun Vantasin <kittinun.f@gmail.com>
2018-05-10 17:38:51 +0900

    :white_check_mark: Add tests (#2)

--- readerk/build.gradle.kts
+++ readerk/build.gradle.kts
@@ -1,9 +1,28 @@
 plugins {
     kotlin("jvm")
+
+    jacoco
+}
+
+repositories {
+    mavenCentral()
 }
 
 dependencies {
     implementation(kotlin("stdlib", "1.2.41"))
 
     testImplementation("junit:junit:4.12")
+}
+
+jacoco {
+    toolVersion = "0.8.1"
+}
+
+tasks {
+    "jacocoTestReport"(JacocoReport::class) {
+        reports {
+            html.isEnabled = false
+            xml.isEnabled = true
+        }
+    }
 }
--- readerk/src/test/java/com/merpay/readerk/ReaderTest.kt
+++ readerk/src/test/java/com/merpay/readerk/ReaderTest.kt
@@ -1,18 +1,65 @@
 package com.merpay.readerk
 
 import org.hamcrest.CoreMatchers.equalTo
-
 import org.junit.Assert.assertThat
 import org.junit.Test
 
 class ReaderTest {
 
     @Test
-    fun test_pure() {
-        val reader = Reader.pure<String, Int>(5)
+    fun pure() {
+        val just5Reader = Reader.pure<String, Int>(5)
 
-        val value = reader.read("1234")
+        assertThat(just5Reader.read("1234"), equalTo(5))
+    }
 
-        assertThat(value, equalTo(5))
+    @Test
+    fun local() {
+        val countLengthReader = Reader { s: String -> s.length }
+
+        val intToCountLengthReader = countLengthReader.local { i: Int -> i.toString() }
+
+        assertThat(intToCountLengthReader.read(489), equalTo(3))
+    }
+
+    @Test
+    fun ask() {
+        val identityReader = Reader.ask<Boolean>()
+
+        assertThat(identityReader.read(true), equalTo(true))
+    }
+
+    @Test
+    fun map1() {
+        //find absolute
+        val originalReader = Reader { i: Int -> if (i > 0) i - 0 else 0 - i }
+
+        //count the digit of absoluted value
+        val newReader = originalReader.map { it.toString().length }
+
+        assertThat(newReader.read(34), equalTo(2))
+        assertThat(newReader.read(-458), equalTo(3))
+    }
+
+    @Test
+    fun map2() {
+        //generate item of x, y items
+        val originalReader = Reader<Pair<Int, Int>, IntArray> { (x, y) -> IntArray(y) { x } }
+
+        //map them to be the sum of all number
+        val newReader = originalReader.map { it.reduce { acc, item -> acc + item } }
+
+        assertThat(newReader.read(3 to 5), equalTo(15))
+    }
+
+    @Test
+    fun flatMap() {
+        //identiy list of string reader
+        val originalReader = Reader.ask<List<String>>()
+
+        //flatMap so it changes to join of all of items
+        val newReader = originalReader.flatMap { Reader { l: List<String> -> l.joinToString("&") } }
+
+        assertThat(newReader.read(listOf("1", "2", "3")), equalTo("1&2&3"))
     }
 }
