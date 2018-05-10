7b3748891b3507c0ba4b4db0818c7366adf7da43
Kittinun Vantasin <kittinun.f@gmail.com>
2018-05-10 16:37:00 +0900

    Add codecov another attempt

--- readerk/build.gradle.kts
+++ readerk/build.gradle.kts
@@ -1,10 +1,18 @@
 plugins {
     kotlin("jvm")
-    id("jacoco-maven-plugin")
+
+    id("jacoco")
 }
 
 dependencies {
     implementation(kotlin("stdlib", "1.2.41"))
 
     testImplementation("junit:junit:4.12")
+}
+
+jacocoTestReport {
+    reports {
+        xml.enabled true
+        html.enabled false
+    }
 }
