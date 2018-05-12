75faf4e30b56df251cf004e2581be64364cabcf6
Kittinun Vantasin <kittinun.f@gmail.com>
2018-05-12 16:06:51 +0900

    :pencil2: Rename the function to runReader (#5)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,9 +6,8 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:3.1.2")
-        classpath(kotlin("gradle-plugin", "1.2.41"))
-        classpath("com.novoda:bintray-release:0.8.0")
+        classpath(kotlin("gradle-plugin", extra.get("kotlinVersion") as String))
+        classpath("com.novoda:bintray-release:${extra.get("bintrayReleaseVersion")}")
     }
 }
 
